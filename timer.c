d31c0911e12bebbc24c45fe033d75f8c1c202dac
Solchild <Soulchild136@gmail.com>
2019-12-06 22:34:47 -0500

    Update timer.c

--- timer.c
+++ timer.c
@@ -1,13 +1,28 @@
-#include "timer.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
 
-int alarms = 0;
-void increment_alarm(){
-  alarms++;
+int hold = 0;
+int time = 0;
+
+void handler1(){
+  hold = 1;
 }
-void sigint_handler(int signum){
-  printf("Alarms occured: %d\n", alarms);
-  printf("The program executed for %d seconds\n", alarms);
-  exit(0);
+void handler2(int signum){
+  printf("Ran for %d seconds", time);
+  exit(1);
+}
+
+int main(int argc, char * argv[]){
+  signal(SIGINT, handler2);
+  while(1){
+    signal(SIGINTALRM, handler1);
+    alarm(1);
+    while(!hold);
+    time++;
+    hold = 0;
+  }
+  
+  return 0;
 }
